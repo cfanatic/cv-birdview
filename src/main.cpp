3838aad02c5db01c57c787f3fd9acc5686d2d9dd
Arnd <git@codefanatic.de>
2018-11-05 21:57:51 +0100

    Add Boost support

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <boost/version.hpp>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::cout << cv::getBuildInformation() << std::endl;
+    std::cout << "Using OpenCV: " << cv::getBuildInformation() << std::endl;
+    std::cout << "Using Boost: " << BOOST_LIB_VERSION << std::endl;
     return 0;
 }
