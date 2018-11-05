32407c343a65d6a03f67cd95a7921264c01f9031
Arnd <git@codefanatic.de>
2018-11-05 23:21:28 +0100

    Adjust main() with configuration tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
-#include <iostream>
-#include <boost/version.hpp>
-#include <opencv2/opencv.hpp>
+#include "configuration.hpp"
+#include "birdview.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Using OpenCV: " << cv::getBuildInformation() << std::endl;
-    std::cout << "Using Boost: " << BOOST_LIB_VERSION << std::endl;
+    // std::cout << "Using OpenCV: " << cv::getBuildInformation() << std::endl;
+    // std::cout << "Using Boost: " << BOOST_LIB_VERSION << std::endl;
+
+    Configuration config;
+
+    std::cout << config.get(Configuration::INPUT) << std::endl;
+    std::cout << config.get(Configuration::OUTPUT) << std::endl;
+
     return 0;
 }
